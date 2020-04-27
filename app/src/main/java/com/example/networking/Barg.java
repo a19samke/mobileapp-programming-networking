a53bfb9e8a51e44b1dcb83482eafff0638772049
samuel <a19samke@student.his.se>
2020-04-27 11:51:56 +0200

    Java class med skapde variabler

--- app/src/main/java/com/example/networking/Barg.java
+++ app/src/main/java/com/example/networking/Barg.java
@@ -1,5 +1,51 @@
 package com.example.networking;
 
-public class Barg {
+import androidx.annotation.NonNull;
 
+public class Barg {
+    //variabler
+    private String name;
+    private String  type;
+    private String company;
+    private String location;
+    private int size;
+
+public Barg(String inName, String inType, String inCompany, String inLocation,int inSize){
+    name=inName;
+    type=inType;
+    company=inCompany;
+    location=inLocation;
+    size=inSize;
+}
+public Barg(String inName){
+    name=inName;
+    company="";
+    type="bro";
+    location="";
+    size=8611;
+
+}
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name;
+    }
+    public String info(){
+    String str=name;
+    str+="The Karakoram range";
+    str+=location;
+    str+="8611";
+    str+=Integer.toString(size);
+    str+="B.";
+    return str;
+
+
+    }
+    public void setSize(int newSize){
+    size=newSize;
+    location="";
+    size=4478;
+
+    }
 }
