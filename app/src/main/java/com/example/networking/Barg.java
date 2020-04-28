cd2afbd8ba25de6c0c465a275dd69d2379f9b9c4
samuel <a19samke@student.his.se>
2020-04-28 11:18:22 +0200

    reformat-code

--- app/src/main/java/com/example/networking/Barg.java
+++ app/src/main/java/com/example/networking/Barg.java
@@ -5,26 +5,21 @@ import androidx.annotation.NonNull;
 public class Barg {
     //variabler
     private String name;
-    private String  type;
-    private String company;
     private String location;
     private int size;
 
-public Barg(String inName, String inType, String inCompany, String inLocation,int inSize){
-    name=inName;
-    type=inType;
-    company=inCompany;
-    location=inLocation;
-    size=inSize;
-}
-public Barg(String matterhorn, String inName, int i){
-    name=inName;
-    company="";
-    type="bro";
-    location="";
-    size=8611;
+    public Barg(String inName, String inType, String inCompany, String inLocation, int inSize) {
+        name = inName;
+        location = inLocation;
+        size = inSize;
+    }
 
-}
+    public Barg(String matterhorn, String inName, int i) {
+        name = inName;
+        location = "";
+        size = 8611;
+
+    }
 
     public Barg(String k2) {
     }
@@ -34,21 +29,22 @@ public Barg(String matterhorn, String inName, int i){
     public String toString() {
         return name;
     }
-    public String info(){
-    String str=name;
-    str+="The Karakoram range";
-    str+=location;
-    str+="8611";
-    str+=Integer.toString(size);
-    str+="B.";
-    return str;
 
+    public String info() {
+        String str = name;
+        str += "The Karakoram range";
+        str += location;
+        str += "8611";
+        str += Integer.toString(size);
+        str += "B.";
+        return str;
 
     }
-    public void setSize(int newSize){
-    size=newSize;
-    location="";
-    size=4478;
+
+    public void setSize(int newSize) {
+        size = newSize;
+        location = "";
+        size = 4478;
 
     }
 }
--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.networking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +28,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    Barg b = new Barg("K2");
-    Barg b2 = new Barg("Matterhorn","Mount Everest" ,8884);
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+        private Object progressDialog;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+        }
 
 
-
-
+    }
 
 
 }
